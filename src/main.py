7c4ad27b98c27da136fc2c138317c37b1185b586
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-15 21:33:27 +0000

    SDFASDFA

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ active_connections: Dict[str, List[WebSocket]] = {} # array of active connection
 @app.get("/test")
 async def main():
 
-    print(active_connections["1000000000000000000"].count)
+    #print(active_connections["1000000000000000000"].count())
 
     for connection in active_connections["1000000000000000000"]:
         await connection.send_text("Magna vola")
