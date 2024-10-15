b893f5af6f9730c1c78489af1257267484a76fec
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-10-15 20:39:34 +0000

    logger fixed

--- src/main.py
+++ src/main.py
@@ -28,6 +28,7 @@ active_connections: Dict[str, List[WebSocket]] = {} # array of active connection
 @app.websocket("/ws/{user_id}/{api_key}")
 async def websocket_endpoint(user_id:str, api_key:str, websocket: WebSocket): # user_id used for connection, api_key to check if user is valid
 
+  logWSConnection(user_id)
   confirmation = (database.get_userHandle_from_apiKey(api_key) == database.user_group_channel_fromID_toHandle(user_id))
 
   if not confirmation:
