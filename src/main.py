b4e996c19c2224c7f7c56caaf01e5a339986ca07
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-11-09 17:58:13 +0000

    fixed an Exception

--- src/main.py
+++ src/main.py
@@ -121,8 +121,8 @@ async def websocket_endpoint(user_id:str, api_key:str, websocket: WebSocket): # 
                 logWSMessage(user_id,"Risposta inviata: "+response+" \n Per richiesta: "+data)
                 await websocket.send_text(response)
 
-            except:
-                logWSMessage(user_id,"Messaggio invalido: "+data)
+            except Exception as e:
+                logWSMessage(user_id,"Messaggio invalido: "+data+" || con errore"+str(e))
                 pass
 
   except WebSocketDisconnect:
