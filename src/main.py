56cfa2da272619b891315380b5b2fee8a1db9ffd
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2024-11-09 18:33:42 +0000

    other dict fixed

--- src/main.py
+++ src/main.py
@@ -123,7 +123,7 @@ async def websocket_endpoint(user_id:str, api_key:str, websocket: WebSocket): # 
                 await websocket.send_text(response)
 
             except Exception as e:
-                logWSMessage(user_id,"Messaggio invalido: "+data+" || con errore: "+str(e))
+                logWSMessage(user_id,"Messaggio invalido: "+str(data)+" || con errore: "+str(e))
                 pass
 
   except WebSocketDisconnect:
